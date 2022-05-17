a65b084cc7e220a2831697c9403c86c3a7547b17
unknown <shariquegithubweb@gmail.com>
2022-05-17 22:53:21 +0500

    asdf

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <style>
-          {/* @import url('https://fonts.googleapis.com/css2?family=Water+Brush&display=swap'); */}
-        </style>
-        <style>
-          @import url('https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap');
-        </style>
+
         <title>Website for a fast food restaurant</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" description="This is a website created for a fast food restaurant"/>
         <script>
           var viewport = document.querySelector("meta[name=viewport]");
           viewport.setAttribute(
           "content",
-          viewport.content + ", height=" + screenWidth.height
+          viewport.content + ", height=" + `${screenWidth.height}`
           );
         </script>
       </Head>
--- pages/_document.js
+++ pages/_document.js
@@ -5,17 +5,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html lang="en">
                 <Head>
+                <title>Website for a fast food restaurant</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" description="This is a website created for a fast food restaurant"/>
                     <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap"
                         rel="stylesheet"
                     />
-                    <link href="https://fonts.googleapis.com/css2?family=Water+Brush&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Kristi&display=swap" rel="stylesheet"/>
                     
                 </Head>
                 <body>
+                    
                     <Main />
                     <NextScript />
                 </body>
